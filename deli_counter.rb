31a8e51e79b8c59b3ecc42553e91edcac18b41b4
Mark Cascardi <mcascardi@gmail.com>
2020-04-16 02:30:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,21 @@
-def line(any_deli)
-  current_line_str = "The line is currently:"
+def line(deli)
+  current_line = "The line is currently:"
   any_deli.each_with_index do |customer, index|
-    current_line_str << " #{index + 1}. #{customer}"
+    current_line << " #{index + 1}. #{customer}"
   end
   if any_deli.size == 0
     puts "The line is currently empty."
   else
-    puts current_line_str
+    puts current_line
   end
 end
 
-def take_a_number(any_deli, new_customer)
+def take_a_number(deli, new_customer)
   any_deli << new_customer
   puts "Welcome, #{new_customer}. You are number #{any_deli.size} in line."
 end
 
-def now_serving(any_deli)
+def now_serving(deli)
   if any_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
